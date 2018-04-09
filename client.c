c73bce8a3976cf051c345163560d60b33218e49c
rox45 <roxas.mrn5@gmail.com>
2018-04-09 18:22:34 -0500

    More comments

--- client.c
+++ client.c
@@ -17,43 +17,68 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
+    int sockfd; //File descriptor in file descriptor table, stores value returned by socket system call
+    int portno; //Port number for server to accept connections
+    int n;  //return value for read and write calls
+    struct sockaddr_in serv_addr; //struct containing server address
     struct hostent *server;
 
     char buffer[256];
+
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+       exit(1);
     }
+
     portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+
+    //Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);   //AF_INET: address domain of socket; SOCK_STREAM: type of socket (specifies UDP or TCP); 0: Protocol
+
+    if (sockfd < 0)
         error("ERROR opening socket");
+
     server = gethostbyname(argv[1]);
+
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+        exit(1);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));  //Clear address structure
+
+     //Setup the host_addr structure for use in bind call
+     //Server byte order
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
+
+    bcopy((char *)server->h_addr,
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
+
     serv_addr.sin_port = htons(portno);
+    
+    //Connect to server
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
+    
+
     printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd, buffer, strlen(buffer));
+    
+    bzero(buffer,256);  //Clear/initialize buffer to 0
+    fgets(buffer,255,stdin);    //Write input to buffer
+
+    n = write(sockfd, buffer, strlen(buffer));  //Write message to server
+
     if (n < 0) 
          error("ERROR writing to socket");
     bzero(buffer,256);
+
     n = read(sockfd, buffer, 255);
+
     if (n < 0) 
          error("ERROR reading from socket");
     printf("%s\n", buffer);
+
     close(sockfd);
     return 0;
 }
--- server.c
+++ server.c
@@ -17,61 +17,53 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
+     int sockfd, newsockfd;    //File descriptors in file descriptor table, stores value returned by socket system call
+     int portno;    //Port number for server to accept connections
+     struct sockaddr_in serv_addr, cli_addr;    //struct containing server address
+     int n; //return value for read and write calls
      socklen_t clilen;
      char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
+
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     // create a socket
-     // socket(int domain, int type, int protocol)
-     sockfd =  socket(AF_INET, SOCK_STREAM, 0);
+
+     //Create socket
+     sockfd =  socket(AF_INET, SOCK_STREAM, 0); //AF_INET: address domain of socket; SOCK_STREAM: type of socket (specify UDP or TCP); 0: Protocol
      if (sockfd < 0) 
         error("ERROR opening socket");
 
-     // clear address structure
+     //Clear address structure
      bzero((char *) &serv_addr, sizeof(serv_addr));
 
      portno = atoi(argv[1]);
 
-     /* setup the host_addr structure for use in bind call */
-     // server byte order
+     //Setup the host_addr structure for use in bind call
+     //Server byte order
      serv_addr.sin_family = AF_INET;  
 
-     // automatically be filled with current host's IP address
+     //Fill with current host's IP address
      serv_addr.sin_addr.s_addr = INADDR_ANY;  
 
-     // convert short integer value for port must be converted into network byte order
+     //Convert short integer value for port must be converted into network byte order
      serv_addr.sin_port = htons(portno);
 
-     // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
-     // bind() passes file descriptor, the address structure, 
-     // and the length of the address structure
-     // This bind() call will bind  the socket to the current IP address on port, portno
+     //Bind the socket to the address
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
 
-     // This listen() call tells the socket to listen to the incoming connections.
-     // The listen() function places all incoming connection into a backlog queue
-     // until accept() call accepts the connection.
-     // Here, we set the maximum size for the backlog queue to 5.
+     //Listen for incoming connections with a backlog queue of 5
      listen(sockfd,5);
 
-     // The accept() call actually accepts an incoming connection
      clilen = sizeof(cli_addr);
 
-     // This accept() function will write the connecting client's address info 
-     // into the the address structure and the size of that structure is clilen.
-     // The accept() returns a new socket file descriptor for the accepted connection.
-     // So, the original socket file descriptor can continue to be used 
-     // for accepting new connections while the new socker file descriptor is used for
-     // communicating with the connected client.
+    /* accept() blocks until client connects to server, returns file descriptor which */
+    /* should be used for all communications on this connection  */
      newsockfd = accept(sockfd, 
                  (struct sockaddr *) &cli_addr, &clilen);
+
      if (newsockfd < 0) 
           error("ERROR on accept");
 
@@ -79,9 +71,10 @@ int main(int argc, char *argv[])
             inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
 
 
-     // This send() function sends the 13 bytes of the string to the new socket
+     //This send() function sends the 13 bytes of the string to the new socket
      send(newsockfd, "Hello, world!\n", 13, 0);
 
+     //Clear/initialize buffer to 0
      bzero(buffer,256);
 
      n = read(newsockfd,buffer,255);
